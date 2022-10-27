fbc61385cb04b3f1e20490deb9c120dccd3b8a54
diogotvf7 <up202108752@g.uporto.pt>
2022-10-27 23:48:57 +0100

    Added ScheduleManager class

---
+++ src/ScheduleManager.cpp
@@ -0,0 +1,53 @@
+//
+// Created by diogotvf7 on 27-10-2022.
+//
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <algorithm>
+
+#include "ScheduleManager.h"
+
+using namespace std;
+
+vector<Class> ScheduleManager::getClasses() const {
+    return classes;
+}
+
+void ScheduleManager::readClassesFile() {
+
+    ifstream in("../data/input/classes.csv");
+    string line;
+
+    getline(in, line); // Ignore first line
+
+    while (getline(in, line, '\r')) {
+
+        string classCode, ucCode, weekday, start, duration, type;
+
+        stringstream input(line);
+
+        getline(input, classCode, ',');
+        getline(input, ucCode, ',');
+        getline(input, weekday, ',');
+        getline(input, start, ',');
+        getline(input, duration, ',');
+        getline(input, type);
+
+        cout << classCode << ' ' << ucCode << ' '  << weekday << ' '  << start << ' '  << duration << ' '  << type;
+
+        std::vector<Class>::iterator i = find_if(classes.begin(), classes.end(),[classCode, ucCode](Class c) { return c.getClassCode() == classCode && c.getUcCode() == ucCode; });
+
+        if (i == classes.end()) {
+
+            Class c(classCode, ucCode);
+            c.addSlot(Slot(weekday, start, duration, type));
+            classes.push_back(c);
+
+        } else {
+
+            i->addSlot(Slot(weekday, start, duration, type));
+        }
+    }
+}
---
+++ src/ScheduleManager.h
@@ -0,0 +1,25 @@
+//
+// Created by diogotvf7 on 27-10-2022.
+//
+
+#ifndef AED_TP1_SCHEDULEMANAGER_H
+#define AED_TP1_SCHEDULEMANAGER_H
+
+#include <vector>
+#include "Class.h"
+
+class ScheduleManager {
+
+public:
+
+    std::vector<Class> getClasses() const;
+
+    void readClassesFile();
+
+private:
+
+    std::vector<Class> classes;
+};
+
+
+#endif //AED_TP1_SCHEDULEMANAGER_H
