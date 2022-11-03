e6cb12f135c43d7093ab72a3512842483fd9ff44
João Belchior <76122976+b3lchior@users.noreply.github.com>
2022-11-03 20:06:16 +0000

    Updated RemoveRequest

--- src/RemoveRequest.cpp
+++ src/RemoveRequest.cpp
@@ -5,6 +5,23 @@
 #include "RemoveRequest.h"
 
 RemoveRequest::RemoveRequest(Student *s, Class *c) {
-    this->s = s;
-    this->c = c;
+    this->student = s;
+    this->intended = c;
+}
+
+Student *RemoveRequest::getStudent() const {
+    return student;
+}
+
+Class *RemoveRequest::getIntendedClass() const {
+    return intended;
+}
+
+bool RemoveRequest::isPossible() {
+
+    unsigned tmpMax = intended->getUc()->getMaxClassStudents();
+    unsigned maxStudents = tmpMax + (intended->countStudents() == tmpMax);
+    unsigned minStudents = std::min(intended->getUc()->getMinClassStudents(intended), intended->countStudents() + 1);
+    if (maxStudents - minStudents >= 4 && intended->countStudents() >= maxStudents - 4) return false;
+    return true;
 }
